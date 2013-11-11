c2b6785f8809abff44fb03e404db0684e9fcca15
James Henderson <james@pcr.me.uk>
2013-11-11 21:00:40 +0000

    Initial commit

---
+++ project.clj
@@ -0,0 +1,43 @@
+(defproject drone ""
+
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  
+  :dependencies [[org.clojure/clojure "1.5.1"]
+
+                 [ring/ring-core "1.2.0"]
+                 [compojure "1.1.5"]
+                 [hiccup "1.0.4"]
+
+                 [prismatic/dommy "0.1.1"]
+
+                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
+                 [org.clojure/clojurescript "0.0-2014"]
+                 [org.clojure/tools.reader "0.7.10"]
+
+                 [clj-drone "0.1.8"]
+                 [jarohen/chord "0.2.0"]]
+
+  :plugins [[jarohen/lein-frodo "0.2.3"]
+            [lein-cljsbuild "1.0.0-alpha2"]
+            [lein-pdo "0.1.1"]
+
+            [lein-localrepo "0.5.2"]]
+
+  :frodo/config-resource "drone-config.edn"
+
+  :source-paths ["src/clojure"]
+
+  :offline? true
+
+  :resource-paths ["resources" "target/resources"]
+
+  :cljsbuild {:builds [{:source-paths ["src/cljs"]
+                        :compiler {:output-to "target/resources/js/drone.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]}
+
+  :aliases {"dev" ["pdo" "cljsbuild" "auto," "frodo"]
+            "start" ["do" "cljsbuild" "once," "trampoline" "frodo"]})
